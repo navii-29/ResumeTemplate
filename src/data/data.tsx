1c9d7f1fbd8ba0a8fd81a78712fd958252c4a744
Shudanshu <sanatanwarrior108@gmail.com>
2026-03-12 00:42:21 +0530

    fix

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -10,7 +10,7 @@ import {
 
 import GithubIcon from '../components/Icon/GithubIcon';
 import InstagramIcon from '../components/Icon/InstagramIcon';
-import kaggleIcon from '../components/Icon/Kaggle'
+import kaggleIcon from '../components/Icon/Kaggle';
 import LinkedInIcon from '../components/Icon/LinkedInIcon';
 // import StackOverflowIcon from '../components/Icon/StackOverflowIcon';
 import TwitterIcon from '../components/Icon/TwitterIcon';
@@ -46,7 +46,7 @@ import {
  */
 export const homePageMeta: HomepageMeta = {
   title: 'My Portfolio Website',
-  description: "This is my Portfolio website you can find here everything about my professional career.",
+  description: 'This is my Portfolio website you can find here everything about my professional career.',
 };
 
 /**
@@ -74,13 +74,14 @@ export const heroData: Hero = {
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        I'm a Mumbai based <strong className="text-stone-100">Python Developer and Data Scientist</strong>, recently worked
-        at <strong className="text-stone-100">Cogoport as Automation Developer</strong> build Workflow for freights booking till freights delivery across International Markets.
+        I'm a Mumbai based <strong className="text-stone-100">Python Developer and Data Scientist</strong>, recently
+        worked at <strong className="text-stone-100">Cogoport as Automation Developer</strong> build Workflow for
+        freights booking till freights delivery across International Markets.
       </p>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
         In my free time time, you can catch me training in <strong className="text-stone-100">Calisthenics</strong>,
-        playing my <strong className="text-stone-100">playing retro games</strong>, or exploring beautiful { 'Spiritual Books'}
-        
+        playing my <strong className="text-stone-100">playing retro games</strong>, or exploring beautiful{' '}
+        Spiritual Books
       </p>
     </>
   ),
@@ -200,35 +201,40 @@ export const skills: SkillGroup[] = [
 export const portfolioItems: PortfolioItem[] = [
   {
     title: 'Web Digest AI',
-    description: 'An LLM-powered web application that generates concise digest-style summaries of websites \
+    description:
+      'An LLM-powered web application that generates concise digest-style summaries of websites \
 Users can register, submit a webpage URL, and receive an AI-generated summary of the page content.',
     url: 'https://github.com/navii-29/WebDigestAI',
     image: porfolioImage1,
   },
   {
     title: 'Mail Shiled',
-    description: 'MailShield is an end-to-end machine learning web application designed to \
+    description:
+      'MailShield is an end-to-end machine learning web application designed to \
     detect phishing and spam emails using Natural Language Processing (NLP) and supervised learning.',
     url: 'https://github.com/navii-29/MailShield',
     image: porfolioImage2,
   },
   {
     title: 'Next Word Predictor',
-    description: 'This project implements a Next Word Prediction system using a Long Short-Term Memory (LSTM) neural network.\
-Given an incomplete sentence, the model predicts the most likely next word based on learned language patterns.' ,
+    description:
+      'This project implements a Next Word Prediction system using a Long Short-Term Memory (LSTM) neural network.\
+Given an incomplete sentence, the model predicts the most likely next word based on learned language patterns.',
     url: 'https://github.com/navii-29/NextWordPredictor',
     image: porfolioImage3,
   },
   {
     title: 'Netflix Data Analysis Dashboard',
-    description: 'An interactive Tableau dashboard analyzing Netflix’s global content trends using a dataset of 6K+ titles.This project explores Netflix’s catalog to uncover insights \
+    description:
+      'An interactive Tableau dashboard analyzing Netflix’s global content trends using a dataset of 6K+ titles.This project explores Netflix’s catalog to uncover insights \
     into:Content distribution by type, genre, rating, and country Growth of movies and TV shows over time Top genres and audience ratings',
     url: 'https://github.com/navii-29/Dashboards',
     image: porfolioImage4,
   },
   {
     title: 'Image Classification',
-    description: 'A production-style RESTful API for image classification built using Flask, InceptionV3, MongoDB, and Docker.\
+    description:
+      'A production-style RESTful API for image classification built using Flask, InceptionV3, MongoDB, and Docker.\
 This project focuses on understanding the complete backend + ML deployment workflow — from authentication and token control to\
  model inference and containerized deployment.',
     url: 'https://github.com/navii-29/ImgClassifyApi',
@@ -236,21 +242,24 @@ This project focuses on understanding the complete backend + ML deployment workf
   },
   {
     title: 'Multiple Linear Regression -- California Housing Price Prediction',
-    description: 'This project demonstrates Multiple Linear Regression using the California Housing dataset from Scikit-Learn. It includes data loading, \
+    description:
+      'This project demonstrates Multiple Linear Regression using the California Housing dataset from Scikit-Learn. It includes data loading, \
     exploratory data analysis (EDA), preprocessing, model training, evaluation, and saving the model using Pickle.',
     url: 'https://github.com/navii-29/California_house_prediction',
     image: porfolioImage6,
   },
   {
     title: '🚗 CarDekho Used Car Price Prediction',
-    description: 'This project focuses on predicting the selling price of used cars using the CarDekho dataset.A Random Forest Regressor is trained after proper data cleaning, preprocessing, and feature engineering.\
+    description:
+      'This project focuses on predicting the selling price of used cars using the CarDekho dataset.A Random Forest Regressor is trained after proper data cleaning, preprocessing, and feature engineering.\
     Project Overview Problem Type: Regression Algorithm Used: Random Forest Regressor Goal: Predict car selling prices based on features like year, fuel type, transmission, owner, etc.',
     url: 'https://github.com/navii-29/Random_forest',
     image: porfolioImage7,
   },
   {
     title: 'Plagiarism',
-    description: 'A production-style REST API that detects text similarity using  NLP similarity scoring  \
+    description:
+      'A production-style REST API that detects text similarity using  NLP similarity scoring  \
     with secure user authentication, token-based usage control, and Dockerized deployment.Built as a backend-focused\
      NLP project demonstrating secure API design, database integration, and containerized deployment',
     url: 'https://github.com/navii-29/PlagiarismAPI/',
@@ -258,7 +267,8 @@ This project focuses on understanding the complete backend + ML deployment workf
   },
   {
     title: 'Customer Churn Prediction App',
-    description: 'Streamlit web application for predicting customer churn probability using a trained TensorFlow ANN model.\
+    description:
+      'Streamlit web application for predicting customer churn probability using a trained TensorFlow ANN model.\
      Deployed from your churn classification project with churn.csv dataset. ppl-ai-file-upload.s3.amazonaws\
       Loads pre-trained model (model.h5) and preprocessing artifacts for real-time predictions.',
     url: 'https://github.com/navii-29/churn_classification',
@@ -266,14 +276,16 @@ This project focuses on understanding the complete backend + ML deployment workf
   },
   {
     title: 'Movie review analyser',
-    description: 'A web application that predicts the sentiment of movie reviews using a deep learning model built with TensorFlow/Keras and deployed with Streamlit.\
+    description:
+      'A web application that predicts the sentiment of movie reviews using a deep learning model built with TensorFlow/Keras and deployed with Streamlit.\
     The model analyzes text reviews and classifies them as Positive or Negative based on learned patterns from the IMDB movie review dataset.',
     url: 'https://github.com/navii-29/ReviewAnalyser',
     image: porfolioImage10,
   },
   {
     title: 'Bank Transaction Api',
-    description: 'A secure RESTful banking API built with Flask, Flask-RESTful, and MongoDB. Supports user registration, account management, transfers, loans, \
+    description:
+      'A secure RESTful banking API built with Flask, Flask-RESTful, and MongoDB. Supports user registration, account management, transfers, loans, \
     and balance inquiries with bcrypt password hashing and transaction fees.',
     url: 'https://github.com/navii-29/TransactionApi',
     image: porfolioImage11,
@@ -286,15 +298,25 @@ This project focuses on understanding the complete backend + ML deployment workf
 export const education: TimelineItem[] = [
   {
     date: 'March 2007 - March 2021',
-    location: 'Little Angels\' Hr. Sec. School,Kathua, Jammu and Kashmir',
+    location: "Little Angels' Hr. Sec. School,Kathua, Jammu and Kashmir",
     title: 'High School',
-    content: <p>Completed my High School with major in Mathematics and Computer Science , there I learnt very profound things that is and will help in life , like interactions , punctuality , honesty , being respectful to others.</p>,
+    content: (
+      <p>
+        Completed my High School with major in Mathematics and Computer Science , there I learnt very profound things
+        that is and will help in life , like interactions , punctuality , honesty , being respectful to others.
+      </p>
+    ),
   },
   {
     date: 'May 2021 - May 2025',
     location: 'Veermata Jijabia technological Institute,Mumbai,Maharashtra',
     title: 'B.Tech in Electronics and Telecommunications Engineering',
-    content: <p>Completed my graduation as an ENTC engineer with a Minor in Data Science,where I learnt about Communications Technologies,Control Systems,Coding with Python.</p>,
+    content: (
+      <p>
+        Completed my graduation as an ENTC engineer with a Minor in Data Science,where I learnt about Communications
+        Technologies,Control Systems,Coding with Python.
+      </p>
+    ),
   },
 ];
 
@@ -305,8 +327,9 @@ export const experience: TimelineItem[] = [
     title: 'Workflow Automation Developer',
     content: (
       <p>
-      • Built automated workflows to streamline freight booking processes, along with their whole journey from source to destination location and standardize business tasks.
-      • Wrote Techinial Documentation on how to build Automated Workflows as task given so that upcoming developer can refer that while building workflows. 
+        • Built automated workflows to streamline freight booking processes, along with their whole journey from source
+        to destination location and standardize business tasks. • Wrote Techinial Documentation on how to build
+        Automated Workflows as task given so that upcoming developer can refer that while building workflows.
       </p>
     ),
   },
@@ -316,8 +339,9 @@ export const experience: TimelineItem[] = [
     title: 'Data Scientist',
     content: (
       <p>
-      • Completed customer churn analysis for a utility client (Power Co.) using Python (Pandas, NumPy) and visual analytics.
-      • Engineered and tuned a random forest model achieving 80% recall for churn prediction; delivered concise executive insights.
+        • Completed customer churn analysis for a utility client (Power Co.) using Python (Pandas, NumPy) and visual
+        analytics. • Engineered and tuned a random forest model achieving 80% recall for churn prediction; delivered
+        concise executive insights.
       </p>
     ),
   },
@@ -386,6 +410,10 @@ export const socialLinks: Social[] = [
   // {label: 'Stack Overflow', Icon: StackOverflowIcon, href: 'https://stackoverflow.com/users/8553186/tim-baker'},
   {label: 'Kaggle', Icon: kaggleIcon, href: 'https://www.kaggle.com/shudanshurajput'},
   {label: 'LinkedIn', Icon: LinkedInIcon, href: 'www.linkedin.com/in/shudanshu-rajput-292hk5108'},
-  {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/shudanshu_rajput_16?igsh=MXc0NGVhMGJwZmRhcw=='},
+  {
+    label: 'Instagram',
+    Icon: InstagramIcon,
+    href: 'https://www.instagram.com/shudanshu_rajput_16?igsh=MXc0NGVhMGJwZmRhcw==',
+  },
   {label: 'Twitter', Icon: TwitterIcon, href: 'https://x.com/RajputShud44848'},
 ];
